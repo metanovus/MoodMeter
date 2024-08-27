998c35cba204ecaa5e3fb1b17928ef3d593ddd8f
miakovlev <miakovlev@expertoption.com>
2024-08-27 16:58:50 +0300

    load_messages_from_db

--- dashboard.py
+++ dashboard.py
@@ -4,8 +4,6 @@ import plotly.graph_objs as go
 from lib.postgresql_manager import PostgreSQLConnector
 import hashlib
 import time
-import os
-import subprocess
 
 conn = PostgreSQLConnector()
 
@@ -28,10 +26,43 @@ def authenticate_user(username, password):
         return False
 
 
+def get_user_id(username):
+    query = "SELECT user_id FROM user_credentials WHERE user_id = %s"
+    result = conn.read_data(query, (username,))
+    if result:
+        return result[0][0]
+    else:
+        return None
+
+
+def get_user_chats(user_id):
+    query = "SELECT chat_id FROM user_chat WHERE user_id = %s"
+    result = conn.read_data(query, (user_id,))
+    return [row[0] for row in result]
+
+
+def load_message_data(chat_id):
+    query = """
+        SELECT message_datetime, chat_id, message_label
+        FROM message_analysis
+        WHERE chat_id = %s
+    """
+
+    data = conn.read_data(query, (chat_id,))
+
+    df = pd.DataFrame(data, columns=['date', 'chat_id', 'label'])
+
+    df['date'] = pd.to_datetime(df['date'])
+
+    df.set_index('date', inplace=True)
+
+    df['label_value'] = df['label'].map(mood_map)
+
+    return df
+
+
 if "authenticated" not in st.session_state:
     st.session_state["authenticated"] = False
-if "token_entered" not in st.session_state:
-    st.session_state["token_entered"] = False
 
 if not st.session_state["authenticated"]:
     st.title("Login")
@@ -49,81 +80,80 @@ if not st.session_state["authenticated"]:
         else:
             st.error("Username/password is incorrect")
 
-elif not st.session_state["token_entered"]:
-    st.title("Telegram Bot Configuration")
-
-    token = st.text_input("Enter your Telegram Bot TOKEN", type="password")
-
-    if st.button("Submit TOKEN"):
-        if token:
-            st.session_state["telegram_token"] = token
-            st.session_state["token_entered"] = True
-
-            env = os.environ.copy()
-            env['TELEGRAM_TOKEN'] = token
-            st.session_state['bot_process'] = subprocess.Popen(['python', 'telegram_bot.py'], env=env)
-
-            st.success("Bot started successfully!")
-            time.sleep(2)
-            st.rerun()
-        else:
-            st.error("Please enter a valid Telegram Bot TOKEN.")
-
 else:
-    mood_map = {
-        'POSITIVE': 1,
-        'NEGATIVE': -1,
-        'NEUTRAL': 0
-    }
+    # Получаем user_id для авторизованного пользователя
+    user_id = get_user_id(st.session_state["username"])
+    if user_id is None:
+        st.error("User ID not found.")
+    else:
+        # Получаем список chat_id для этого пользователя
+        user_chats = get_user_chats(user_id)
 
-    df = pd.read_json('generated_records.json')
-    df['date'] = pd.to_datetime(df['date'])
-    df.set_index('date', inplace=True)
+        if not user_chats:
+            st.error("No chats found for this user.")
+        else:
+            # Добавляем фильтр для выбора чата
+            chat_id = st.sidebar.selectbox("Select Chat", options=user_chats)
 
-    df['label_value'] = df['label'].map(mood_map)
+            mood_map = {
+                'POSITIVE': 1,
+                'NEGATIVE': -1,
+                'NEUTRAL': 0
+            }
 
-    st.sidebar.header("Filter by Date")
-    start_date = st.sidebar.date_input("Start date", df.index.min().date())
-    end_date = st.sidebar.date_input("End date", df.index.max().date())
+            df = load_message_data(chat_id)
 
-    filtered_df = df[(df.index.date >= start_date) & (df.index.date <= end_date)]
+            if df.empty:
+                st.warning("No data available for the selected chat.")
 
-    grouping = st.sidebar.selectbox(
-        'Choose interval for grouping:',
-        ['Hours', 'Days', 'Weeks']
-    )
+                fig = go.Figure()
 
-    if grouping == 'Hours':
-        resampled_df = filtered_df.resample('h')['label_value'].mean()
-    elif grouping == 'Days':
-        resampled_df = filtered_df.resample('D')['label_value'].mean()
-    elif grouping == 'Weeks':
-        resampled_df = filtered_df.resample('W')['label_value'].mean()
+                st.title("Mood grouping by time interval")
+                st.plotly_chart(fig)
+            else:
 
-    fig = go.Figure()
+                st.sidebar.header("Filter by Date")
+                start_date = st.sidebar.date_input("Start date", df.index.min().date())
+                end_date = st.sidebar.date_input("End date", df.index.max().date())
 
-    fig.add_trace(go.Bar(
-        x=resampled_df.index,
-        y=resampled_df.clip(lower=0),
-        name='Positive Mood',
-        marker_color='green'
-    ))
+                filtered_df = df[(df.index.date >= start_date) & (df.index.date <= end_date)]
 
-    fig.add_trace(go.Bar(
-        x=resampled_df.index,
-        y=resampled_df.clip(upper=0),
-        name='Negative Mood',
-        marker_color='red'
-    ))
+                grouping = st.sidebar.selectbox(
+                    'Choose interval for grouping:',
+                    ['Hours', 'Days', 'Weeks']
+                )
 
-    fig.update_layout(
-        title=f'Mood over Time ({grouping})',
-        xaxis_title='Time',
-        yaxis_title='Mood Score',
-        showlegend=False,
-        plot_bgcolor='white',
-        barmode='relative'
-    )
+                if grouping == 'Hours':
+                    resampled_df = filtered_df.resample('h')['label_value'].mean()
+                elif grouping == 'Days':
+                    resampled_df = filtered_df.resample('D')['label_value'].mean()
+                elif grouping == 'Weeks':
+                    resampled_df = filtered_df.resample('W')['label_value'].mean()
 
-    st.title(f'Mood grouping by {grouping.lower()}')
-    st.plotly_chart(fig)
+                fig = go.Figure()
+
+                fig.add_trace(go.Bar(
+                    x=resampled_df.index,
+                    y=resampled_df.clip(lower=0),
+                    name='Positive Mood',
+                    marker_color='green'
+                ))
+
+                fig.add_trace(go.Bar(
+                    x=resampled_df.index,
+                    y=resampled_df.clip(upper=0),
+                    name='Negative Mood',
+                    marker_color='red'
+                ))
+
+                fig.update_layout(
+                    title=f'Mood over Time ({grouping})',
+                    xaxis_title='Time',
+                    yaxis_title='Mood Score',
+                    showlegend=False,
+                    plot_bgcolor='white',
+                    barmode='relative'
+                )
+
+                st.title(f'Mood grouping by {grouping.lower()}')
+                st.plotly_chart(fig)
