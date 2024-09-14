2628446837d3c166fd586d0b81f89137c67e30d1
otvet4ikov <otvet4ikov16@gmail.com>
2024-09-14 17:28:15 +0400

    add register user and chat to the bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,8 +1,9 @@
 import os
+import hashlib
 
 from dotenv import load_dotenv
-from telegram import Update
-from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext, MessageHandler, Filters
 
 import mood_calculator
 import transformers_mood
@@ -16,6 +17,8 @@ ADMIN_CHAT_ID = os.environ.get('ADMIN_CHAT_ID')
 
 conn = PostgreSQLConnector()
 
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
 
 def save_message_to_sql(chat_id, user_id, message_text, message_label, label_score, chat_mood, message_datetime,
                         table_name='message_analysis'):
@@ -23,6 +26,16 @@ def save_message_to_sql(chat_id, user_id, message_text, message_label, label_sco
     columns = ['chat_id', 'user_id', 'message_text', 'message_datetime', 'message_label', 'label_score', 'chat_mood']
     conn.insert_data(data, table_name, columns)
 
+
+def save_user_to_sql(id, user_id, password, table_name='user_credentials'):
+    data = [(id, user_id, password)]
+    columns = ['id', 'user_id', 'password']
+    conn.insert_data(data, table_name, columns)
+
+def save_chat_to_sql(user_id, chat_id, table_name='user_chat'):
+    data = [(user_id, chat_id)]
+    columns = ['user_id', 'chat_id']
+    conn.insert_data(data, table_name, columns)
 
 def handle_message(update: Update, context: CallbackContext):
     """
@@ -71,6 +84,41 @@ def handle_message(update: Update, context: CallbackContext):
     print(f"Message: {message_text}")
     print(message_label, label_score, 'MOOD:', chat_mood)
 
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Привет! Используйте команду /add_user, чтобы добавить свой user_id.')
+    update.message.reply_text('Или команду /add_chat, чтобы добавить чат в базу данных')
+
+def add_user(update: Update, context: CallbackContext) -> None:
+    if update.effective_chat.type != 'private':
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Эта команда доступна только в личных сообщениях.')
+        return
+    user_id = update.message.from_user.id
+    password = hash_password(str(user_id))
+    save_user_to_sql(2, user_id, password)
+    update.message.reply_text(f"Ваш user_id: {user_id} был записан")
+
+def add_chat(user_id: int, chat_id: int):
+    save_chat_to_sql(user_id, chat_id)
+    
+
+def add_chat_command(update: Update, context: CallbackContext) -> None:
+    if update.effective_chat.type != 'private':
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Эта команда доступна только в личных сообщениях.')
+        return
+    
+    if len(context.args) != 1:
+        update.message.reply_text('Пожалуйста, укажите ID чата, который нужно добавить.')
+        return
+
+    try:
+        user_id = update.message.from_user.id
+        chat_id = int(context.args[0])  
+    except ValueError:
+        update.message.reply_text('ID чата должен быть числом.')
+        return
+    
+    add_chat(user_id, chat_id)
+    update.message.reply_text(f'Чат с ID {chat_id} добавлен в базу данных.')
 
 def main():
     """
@@ -87,6 +135,9 @@ def main():
     bot = updater.bot
     dispatcher = updater.dispatcher
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('add_user', add_user))
+    dispatcher.add_handler(CommandHandler('add_chat', add_chat_command))
 
     updater.start_polling()
     updater.idle()
